95b9447e4ee527bb90ea2fe0f095d4497417f55a
shaozhiyujiayou <126433981+shaozhiyujiayou@users.noreply.github.com>
2023-06-13 20:02:47 +0800

    Environment_marl.py

--- Environment_marl.py
+++ Environment_marl.py
@@ -6,47 +6,47 @@ import math
 
 
 np.random.seed(1234)
+# 确保在每次运行程序时使用NumPy库生成生成的随机数序列是可重复的
 
+# class V2Vchannels:
+#     # Simulator of the V2V Channels
 
-class V2Vchannels:
-    # Simulator of the V2V Channels
+#     def __init__(self):
+#         self.t = 0
+#         self.h_bs = 1.5
+#         self.h_ms = 1.5
+#         self.fc = 2
+#         self.decorrelation_distance = 10
+#         self.shadow_std = 3
 
-    def __init__(self):
-        self.t = 0
-        self.h_bs = 1.5
-        self.h_ms = 1.5
-        self.fc = 2
-        self.decorrelation_distance = 10
-        self.shadow_std = 3
+#     def get_path_loss(self, position_A, position_B):
+#         d1 = abs(position_A[0] - position_B[0])
+#         d2 = abs(position_A[1] - position_B[1])
+#         d = math.hypot(d1, d2) + 0.001
+#         d_bp = 4 * (self.h_bs - 1) * (self.h_ms - 1) * self.fc * (10 ** 9) / (3 * 10 ** 8)
 
-    def get_path_loss(self, position_A, position_B):
-        d1 = abs(position_A[0] - position_B[0])
-        d2 = abs(position_A[1] - position_B[1])
-        d = math.hypot(d1, d2) + 0.001
-        d_bp = 4 * (self.h_bs - 1) * (self.h_ms - 1) * self.fc * (10 ** 9) / (3 * 10 ** 8)
+#         def PL_Los(d):
+#             if d <= 3:
+#                 return 22.7 * np.log10(3) + 41 + 20 * np.log10(self.fc / 5)
+#             else:
+#                 if d < d_bp:
+#                     return 22.7 * np.log10(d) + 41 + 20 * np.log10(self.fc / 5)
+#                 else:
+#                     return 40.0 * np.log10(d) + 9.45 - 17.3 * np.log10(self.h_bs) - 17.3 * np.log10(self.h_ms) + 2.7 * np.log10(self.fc / 5)
 
-        def PL_Los(d):
-            if d <= 3:
-                return 22.7 * np.log10(3) + 41 + 20 * np.log10(self.fc / 5)
-            else:
-                if d < d_bp:
-                    return 22.7 * np.log10(d) + 41 + 20 * np.log10(self.fc / 5)
-                else:
-                    return 40.0 * np.log10(d) + 9.45 - 17.3 * np.log10(self.h_bs) - 17.3 * np.log10(self.h_ms) + 2.7 * np.log10(self.fc / 5)
+#         def PL_NLos(d_a, d_b):
+#             n_j = max(2.8 - 0.0024 * d_b, 1.84)
+#             return PL_Los(d_a) + 20 - 12.5 * n_j + 10 * n_j * np.log10(d_b) + 3 * np.log10(self.fc / 5)
 
-        def PL_NLos(d_a, d_b):
-            n_j = max(2.8 - 0.0024 * d_b, 1.84)
-            return PL_Los(d_a) + 20 - 12.5 * n_j + 10 * n_j * np.log10(d_b) + 3 * np.log10(self.fc / 5)
+#         if min(d1, d2) < 7:
+#             PL = PL_Los(d)
+#         else:
+#             PL = min(PL_NLos(d1, d2), PL_NLos(d2, d1))
+#         return PL  # + self.shadow_std * np.random.normal()
 
-        if min(d1, d2) < 7:
-            PL = PL_Los(d)
-        else:
-            PL = min(PL_NLos(d1, d2), PL_NLos(d2, d1))
-        return PL  # + self.shadow_std * np.random.normal()
-
-    def get_shadowing(self, delta_distance, shadowing):
-        return np.exp(-1 * (delta_distance / self.decorrelation_distance)) * shadowing \
-               + math.sqrt(1 - np.exp(-2 * (delta_distance / self.decorrelation_distance))) * np.random.normal(0, 3)  # standard dev is 3 db
+#     def get_shadowing(self, delta_distance, shadowing):
+#         return np.exp(-1 * (delta_distance / self.decorrelation_distance)) * shadowing \
+#                + math.sqrt(1 - np.exp(-2 * (delta_distance / self.decorrelation_distance))) * np.random.normal(0, 3)  # standard dev is 3 db
 
 
 class V2Ichannels:
@@ -58,7 +58,7 @@ class V2Ichannels:
         self.h_ms = 1.5
         self.Decorrelation_distance = 50
         self.BS_position = [750 / 2, 1299 / 2]  # center of the grids
-        self.shadow_std = 8
+        self.shadow_std = 8  #BS天线的增益gain
 
     def get_path_loss(self, position_A):
         d1 = abs(position_A[0] - self.BS_position[0])
